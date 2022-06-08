1493b4ef9ddc37c7c45d02297814d14e7022c41b
wanguu <wanguu@example.com>
2022-06-08 23:15:05 +0800

    add settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,6 @@
 import sys
-
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -8,8 +8,11 @@ class AlienInvasion:
         """初始化游戏并创建游戏资源"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode(1200, 800)
+        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
         pygame.display.set_caption("Alien Invasion")
+
+        # 设置背景色
+        self.bg_color = (230,230,230)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -18,7 +21,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            # 每次循环时都重绘屏幕
+            self.screen.fill(self.settings.bg_color)
             # 让最近绘制的屏幕可见
             pygame.display.flip()
 
