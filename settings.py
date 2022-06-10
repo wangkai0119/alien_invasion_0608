5356941825d957252b4212260b10d9502605fad2
wanguu <wanguu@example.com>
2022-06-11 00:49:10 +0800

    Part_12.9 done

--- settings.py
+++ settings.py
@@ -8,7 +8,12 @@ class Settings:
         self.bg_color = (230,230,230)
 
         # 飞船设置
-        self.ship_speed = 1 # init: 1.5
+        self.ship_speed = 0.8 # init: 1.5
 
-
+        # 子弹设置
+        self.bullet_speed = 0.6
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 3
         
