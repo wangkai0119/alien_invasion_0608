2dbdf0c022e55c65392c079a8558bae7fcea2e91
wanguu <wanguu@example.com>
2022-06-11 20:02:02 +0800

    Part_13.3 done

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -16,9 +17,12 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._creat_fleet()  # 创建外星人群
 
         # 设置背景色
-        self.bg_color = (230,230,230)
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -46,7 +50,6 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
-
     def _check_keydown_events(self, event):
         """响应按键"""
         if event.key == pygame.K_RIGHT:
@@ -71,18 +74,44 @@ class AlienInvasion:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
+    def _creat_fleet(self):
+        # 创建外星人群
+        # 创建一个外星人并计算一行可容纳多少个外星人
+        # 外星人的间距为外星人的宽度
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        numbers_alien_x = available_space_x // (2 * alien_width)
+        # 计算屏幕可以容纳多少行外星人
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+        # 创建外星人群
+        for row_number in range(number_rows):
+            # 创建第一行外星人
+            for alien_number in range(numbers_alien_x):
+                self._creat_alien(alien_number, row_number)
+
+    def _creat_alien(self, alien_number, row_number):
+        # 创建第1个外星人，并将其加入当前行。
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.rect.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.y = alien_height + 2 * alien_height * row_number
+        self.aliens.add(alien)
+
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕。"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
+
 
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
     ai = AlienInvasion()
     ai.run_game()
-
-
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
     def __init__(self):
         """初始化游戏的设置"""
         # 屏幕设置
-        self.screen_width = 900 # init: 1200
+        self.screen_width = 1200 # init: 1200
         self.screen_height = 720 # init: 800
         self.bg_color = (230,230,230)
 
